aa4250e9b42f8d3099e61ee6b003fcb82584fde7
Henrypio <apple@APPLEs-MacBook-Air.local>
2024-08-08 12:02:24 +0100

    fix build error 1

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+// next.config.js
+module.exports = {
+  experimental: {
+    appDir: true, // If you are using the new app directory
+  },
+  pageExtensions: ["js", "jsx", "ts", "tsx"],
 };
-
-export default nextConfig;
